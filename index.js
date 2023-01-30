1cc06cefa100c453ad6fcee829e1e7e16a865635
MartyPR <martin.p1131@gmail.com>
2023-01-30 15:50:39 -0500

    agregar menu mobile y ordernes

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const navEmail=document.querySelector('.navbar-email');
 const menuDp=document.querySelector('.desktop-menu');
+
+const menuMb=document.querySelector('.mobile-menu')
+const icoMenu=document.querySelector('.menu');
+
+const cart=document.querySelector('.shopping-cart');
+const detail=document.querySelector('.product-detail');
+
 navEmail.addEventListener('click',()=>{
-    menuDp.classList.toggle('inactive')
+    menuDp.classList.toggle('inactive');
+})
+icoMenu.addEventListener('click',()=>{
+    const isAsideClosed =   detail.classList.contains('inactive')
+   
+    if(!isAsideClosed){
+        detail.classList.add('inactive');
+    }
+    menuMb.classList.toggle('inactive');
+})
+cart.addEventListener('click',()=>{
+    const isMobileMenuClosed = menuMb.classList.contains('inactive');
+   
+
+        if(!isMobileMenuClosed){
+            menuMb.classList.add('inactive');
+        }
+    
+        detail.classList.toggle('inactive');
+    
 })
