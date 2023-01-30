febdf52b602622a42ed3e5122d63202ce3110a49
MartyPR <martin.p1131@gmail.com>
2023-01-30 16:37:45 -0500

    agregar contenido en pagina

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const icoMenu=document.querySelector('.menu');
 const cart=document.querySelector('.shopping-cart');
 const detail=document.querySelector('.product-detail');
 
+const cardContainer=document.querySelector('.cards-container')
+
 navEmail.addEventListener('click',()=>{
     menuDp.classList.toggle('inactive');
 })
@@ -29,3 +31,92 @@ cart.addEventListener('click',()=>{
         detail.classList.toggle('inactive');
     
 })
+
+
+const productList = [];
+productList.push({
+name:'bike',
+price:212,
+image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name:'pantalla',
+    price:512,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+name:'Playstation 5',
+price:120,
+image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+    name:'bike',
+    price:212,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+productList.push({
+        name:'pantalla',
+        price:512,
+        image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+})
+productList.push({
+    name:'Playstation 5',
+    price:120,
+    image:'https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
+});
+
+
+/*<div class="product-card">
+<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="">
+<div class="product-info">
+  <div>
+    <p>$120,00</p>
+    <p>Bike</p>
+  </div>
+  <figure>
+    <img src="./icons/bt_add_to_cart.svg" alt="">
+  </figure>
+</div>
+</div>
+</div>*/
+
+for(product of productList){
+    const profuctCard=document.createElement('div');
+    profuctCard.classList.add('product-card');
+//->
+    const img = document.createElement('img');
+    img.setAttribute('src', product.image );
+
+    const productInfo=document.createElement('div')
+    productInfo.classList.add('product-info');
+//-->
+    const productInfoDiv=document.createElement('div');
+
+
+    const productInfoPrice=document.createElement('p');
+    productInfoPrice.innerText=`$- ${product.price}`;
+
+    const productInfoName=document.createElement('p');
+    productInfoName.innerText=`${product.name}`
+    productInfoDiv.appendChild(productInfoPrice);
+    productInfoDiv.appendChild(productInfoName);
+//-->
+//-->
+
+    const figure = document.createElement('figure');
+    const iconCart= document.createElement('img');
+    iconCart.setAttribute('src','./icons/bt_add_to_cart.svg');
+//-->
+
+    figure.appendChild(iconCart);
+
+    productInfo.appendChild(productInfoDiv)
+    productInfo.appendChild(figure)
+
+    profuctCard.appendChild(img);
+    profuctCard.appendChild(productInfo);
+//->
+//---------->
+    cardContainer.appendChild(profuctCard);
+
+}
